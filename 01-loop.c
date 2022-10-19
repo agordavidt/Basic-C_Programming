dc858346615eeb70838cb37d0fc8a5f8dc0353b8
agordavidt <98212267+agordavidt@users.noreply.github.com>
2022-10-19 23:22:21 +0100

    for loop

---
+++ 01-loop.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - funtion that uses for loop
+ *
+ * return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	for (int i = 0;i < 5;i++)
+	{
+		printf("I love programming\n");
+	}
+	return 0;
+}
