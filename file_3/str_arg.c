db34af347b9ba778c705c2b5e28863110049e1df
agordavidt <98212267+agordavidt@users.noreply.github.com>
2022-11-04 10:42:10 +0100

    reading commnad line

---
+++ file_3/str_arg.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * min - a program to describe strings and arguments to min
+ *
+ * Return: 0
+ */
+int min(void){
+	printf("A program to demonstrate arguments to main\n");
+
+	return (0);
+}
