b6d4de90eec73fdf4e8212f49dc7ef818036f487
agordavidt <98212267+agordavidt@users.noreply.github.com>
2022-11-17 08:08:05 +0100

    example

---
+++ ch-18/18L03.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	enum players {Ronaldo,
+			messi,
+Neymar,
+			Suarez,
+			Benzema,
+			Mpape};
+	printf("Ronaldo is num: %d\n", Ronaldo);
+	printf("Messi is num: %d\n", messi);
+	return (0);
+}
