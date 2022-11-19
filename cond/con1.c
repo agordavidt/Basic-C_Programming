48ae4296de67d0a490a42527f1ee9e259b288370
agordavidt <98212267+agordavidt@users.noreply.github.com>
2022-11-19 22:20:58 +0100

    initial commit

---
+++ cond/con1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+	int a = 1;
+
+	if (a == 1) {
+		printf("My name is David\n");
+	}else {
+		printf("You have entered a wrong number");
+	}
+
+	return (0);
+}
